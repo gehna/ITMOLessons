6409cbe15822dde69503cab728f292e6e4b6ea99
Gennadii Legkodukhov <gehna@yandex.ru>
2021-10-08 19:11:07 +0300

    Legkodukhov Gennadiy

---
+++ src/main/java/com/ifmo/lesson1/ThreeDigitsSum.java
@@ -0,0 +1,24 @@
+package com.ifmo.lesson1;
+
+public class ThreeDigitsSum {
+    /*
+    В переменной n хранится натуральное трёхзначное число.
+    Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n.
+     */
+    public static void main(String[] args) {
+        int n = 123;
+
+        int sum = sum(n);
+
+        System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+        // TODO implement
+        int i = n / 100;
+        int j = n / 10 - i*10;
+        int k = n % 10;
+
+        return i + j + k;
+    }
+}
