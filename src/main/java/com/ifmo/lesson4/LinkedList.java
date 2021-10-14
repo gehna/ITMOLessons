a2b1be7d70ebc31b4750ecaa509b25e0d009df89
Gennadii Legkodukhov <gehna@yandex.ru>
2021-10-14 21:15:24 +0300

    1 attempt lesson 4

---
+++ src/main/java/com/ifmo/lesson4/LinkedList.java
@@ -0,0 +1,92 @@
+package com.ifmo.lesson4;
+
+/**
+ * Односвязный список, где каждый предыдущий
+ * элемент харнит ссылку на следующий. Список
+ * оканчивается ссылкой со значением {@code null}.
+ */
+public class LinkedList {
+    /** Ссылка на первый элемент списка. */
+    private Item head;
+
+    /**
+     * Добавляет значение в конец списка.
+     *
+     * @param val Значение, которое будет добавлено.
+     */
+    public void add(Object val) {
+        // TODO implement
+        Item item = new Item(val);
+        if (head==null){
+            head=item;
+        } else {
+            Item current = head;
+            while (true){
+                if (current.next == null){
+                    current.next = item;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    /**
+     * Извлекает значение из списка по индексу.
+     *
+     * @param i Индекс значения в списке.
+     * @return Значение, которое находится по индексу
+     * или {@code null}, если не найдено.
+     */
+    public Object get(int i) {
+        // TODO implement
+        if (head==null){
+            return null;
+        }
+        int counter = 0;
+        Item current = head;
+        while (true){
+
+            if (counter==i){
+                return current.value;
+            }
+            if (counter<i && current.next==null){
+                return null;
+            }
+            current = current.next;
+            counter++;
+
+        }
+
+    }
+
+    /**
+     * Удаляет значение по индексу и возвращает
+     * удаленный элемент.
+     *
+     * @param i Индекс, по которому будет удален элемент.
+     * @return Удаленное значение или {@code null}, если не найдено.
+     */
+    public Object remove(int i) {
+        // TODO implement
+        if (head==null){
+            return null;
+        }
+        int counter = 0;
+        Item current = head;
+        Item prev = head;
+
+        while (true){
+            if (counter==i){
+                prev.next = current.next;
+                return current;
+            }
+            if (counter<i && current.next==null){
+                return null;
+            }
+            prev=current;
+            current=current.next;
+            counter++;
+        }
+    }
+}
---
+++ src/main/java/com/ifmo/lesson4/Main.java
@@ -0,0 +1,37 @@
+package com.ifmo.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        System.out.println("Empty list");
+        printList(list, 3);
+        //null
+        //null
+        //null
+
+        list.add("row 1");
+        list.add("row 2");
+        list.add("row 3");
+
+        System.out.println("3 items in list");
+        printList(list, 3);
+        //row 1
+        //row 2
+        //row 3
+
+        System.out.println("1 item removed");
+        Object removed = list.remove(1);
+        System.out.println("removed: "+ removed); //row 2
+        printList(list, 3);
+        //row 1
+        //row 3
+        //null
+    }
+    private static void printList(LinkedList list, int len){
+        for (int i = 0; i < len; i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+}
